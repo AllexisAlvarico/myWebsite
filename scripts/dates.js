c5f769a29c0f7a845f5cd8a45adb31c3055eace0
AllexisAlvarico <Allexis_alvarico@outlook.com>
2024-09-06 13:52:31 +0100

    first commit

---
+++ scripts/dates.js
@@ -0,0 +1,8 @@
+let daysInYear = 365;
+let weeksInYear = 52;
+let daysInWeek = 7;
+daysInYear++;
+console.log(daysInYear);
+daysInWeek-=2;
+console.log(daysInWeek * weeksInYear);
+console.log(weeksInYear/12);
---
+++ scripts/theme.js
@@ -0,0 +1,36 @@
+
+function blueText(){
+    let target = document.getElementById("text1");
+    let elt = document.getElementsByTagName('body').item(0);
+    elt.style.backgroundColor = 'blue'; 
+    target.style.color= "blue";
+    target.style.fontFamily = "Impact,Charcoal,sans-serif"
+
+}
+
+function redText(){
+
+    let target = document.getElementById("text1");
+    let elt = document.getElementsByTagName('body').item(0);
+    elt.style.backgroundColor = 'red'; 
+    target.style.color= "red";
+    target.style.fontFamily = "Trebuchet MS, sans-serif"
+}
+
+function greenText(){
+
+    let target = document.getElementById("text1");
+    let elt = document.getElementsByTagName('body').item(0);
+    elt.style.backgroundColor = 'green'; 
+    target.style.color= "green";
+    target.style.fontFamily = "Times New Roman, serif"
+}
+
+function reset(){
+    
+    let target = document.getElementById("text1");
+    let elt = document.getElementsByTagName('body').item(0);
+    elt.style.backgroundColor = 'white'; 
+    target.style.color= "black";
+    target.style.fontFamily = "Times New Roman, serif"
+}
