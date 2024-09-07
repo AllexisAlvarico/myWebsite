64c8e192ba81f1f7ceda2648f42eb45afa4afff6
AllexisAlvarico <c00242855@itcarlow.ie>
2024-09-08 00:47:49 +0100

    added some minor changes (dates and styles)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,37 +9,82 @@ function formCloseBlog(){
 	document.getElementById("myForm").style.display = "none";
 }
 
+
+function setDate(){
+	let currentDate = new Date();
+	let month = "";
+
+	switch(currentDate.getMonth()){
+		case 0:
+				month="January";
+				break;
+			case 1:
+				month="February";
+				break;
+			case 2:
+				month="March";
+				break;
+			case 3:
+				month="April";
+				break;
+			case 4:
+				month="May";
+				break;
+			case 5:
+				month="June";
+				break;
+			case 6:
+				month="July";
+				break;
+			case 7:
+				month="August";
+				break;
+			case 8:
+				month="September";
+				break;
+			case 9:
+				month="October";
+				break;
+			case 10:
+				month="November";
+				break;
+			case 11:
+				month="December";
+				break;
+			default:
+				console.log("Number invalid");
+				break;
+	}
+
+	let setDate = month + " " + currentDate.getDate() + ", "
+	+ currentDate.getFullYear() + " "
+	+ currentDate.getHours() + ":"
+	+ currentDate.getMinutes();
+
+	return setDate;
+}
+
 function setBlog(){
-	var currentDate = new Date();
+
 	let divBlog = document.createElement("div");
 	divBlog.classList.add("card");
-	let titleHeading = document.createElement("h2");
-	let authorHeading = document.createElement("h2");
+	let titleHeading = document.createElement("h5");
+	titleHeading.classList.add("titleBlog");
+	let authorHeading = document.createElement("h5");
+	authorHeading.classList.add("author");
 	let bodyTextField = document.createElement("p");
+	bodyTextField.classList.add("blog");
 	let dateTextField = document.createElement("h5");
+	dateTextField.classList.add("date");
 
-	dateTextField.textContent = currentDate.getDate() + "/"
-	+ (currentDate.getMonth()+1)  + "/"
-	+ currentDate.getFullYear() + " at "
-	+ currentDate.getHours() + ":" + currentDate.getMinutes();
+	dateTextField.textContent = "Date: " + setDate();
 
 	titleHeading.innerHTML = document.getElementById("titleID").value;
-	authorHeading.innerHTML = document.getElementById("authorID").value;
+	authorHeading.innerHTML = "Author: " + document.getElementById("authorID").value;
 	bodyTextField.innerHTML = document.getElementById("bodyTextID").value;
-
-	document.body.appendChild(dateTextField);
 	document.body.appendChild(divBlog);
 	divBlog.appendChild(titleHeading);
 	divBlog.appendChild(authorHeading);
+	divBlog.appendChild(dateTextField);
 	divBlog.appendChild(bodyTextField);
-
-	//document.body.appendChild(authorHeading);
-	//document.body.appendChild(bodyTextField);
-	// get the date and time
-	// create a div
-	// set the title
-	// set the author
-	// create a paragraph
-	// create a image if needed
-	// create a button
 };
