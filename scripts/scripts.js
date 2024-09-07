0abd2553f10c760a1946432d6d50649a9ae20335
AllexisAlvarico <c00242855@itcarlow.ie>
2024-09-08 00:16:41 +0100

    got the create blog post working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,6 @@
-
 function changeMode(){
 
 }
-
 function formOpenBlog(){
 	document.getElementById("myForm").style.display = "block";
 }
@@ -11,19 +9,30 @@ function formCloseBlog(){
 	document.getElementById("myForm").style.display = "none";
 }
 
-function setBlog(t_title, t_author, t_bodyText){
+function setBlog(){
+	var currentDate = new Date();
 	let divBlog = document.createElement("div");
 	divBlog.classList.add("card");
 	let titleHeading = document.createElement("h2");
 	let authorHeading = document.createElement("h2");
 	let bodyTextField = document.createElement("p");
-	titleHeading.innerHTML = "some title";
-	authorHeading.innerHTML = "some author";
-	bodyTextField.innerHTML = "t_bodyText";
+	let dateTextField = document.createElement("h5");
+
+	dateTextField.textContent = currentDate.getDate() + "/"
+	+ (currentDate.getMonth()+1)  + "/"
+	+ currentDate.getFullYear() + " at "
+	+ currentDate.getHours() + ":" + currentDate.getMinutes();
+
+	titleHeading.innerHTML = document.getElementById("titleID").value;
+	authorHeading.innerHTML = document.getElementById("authorID").value;
+	bodyTextField.innerHTML = document.getElementById("bodyTextID").value;
+
+	document.body.appendChild(dateTextField);
 	document.body.appendChild(divBlog);
 	divBlog.appendChild(titleHeading);
 	divBlog.appendChild(authorHeading);
 	divBlog.appendChild(bodyTextField);
+
 	//document.body.appendChild(authorHeading);
 	//document.body.appendChild(bodyTextField);
 	// get the date and time
