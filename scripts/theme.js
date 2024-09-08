4c093f7e300cff9d9efb4462cb570fbb7ff57aad
AllexisAlvarico <c00242855@itcarlow.ie>
2024-09-08 14:05:02 +0100

    added a dark mode in the blogpage

--- scripts/theme.js
+++ scripts/theme.js
@@ -2,17 +2,16 @@
 function blueText(){
     let target = document.getElementById("text1");
     let elt = document.getElementsByTagName('body').item(0);
-    elt.style.backgroundColor = 'blue'; 
+    elt.style.backgroundColor = 'blue';
     target.style.color= "blue";
     target.style.fontFamily = "Impact,Charcoal,sans-serif"
-
 }
 
 function redText(){
 
     let target = document.getElementById("text1");
     let elt = document.getElementsByTagName('body').item(0);
-    elt.style.backgroundColor = 'red'; 
+    elt.style.backgroundColor = 'red';
     target.style.color= "red";
     target.style.fontFamily = "Trebuchet MS, sans-serif"
 }
@@ -21,16 +20,28 @@ function greenText(){
 
     let target = document.getElementById("text1");
     let elt = document.getElementsByTagName('body').item(0);
-    elt.style.backgroundColor = 'green'; 
+    elt.style.backgroundColor = 'green';
     target.style.color= "green";
     target.style.fontFamily = "Times New Roman, serif"
 }
 
 function reset(){
-    
     let target = document.getElementById("text1");
     let elt = document.getElementsByTagName('body').item(0);
-    elt.style.backgroundColor = 'white'; 
+    elt.style.backgroundColor = 'white';
     target.style.color= "black";
     target.style.fontFamily = "Times New Roman, serif"
+}
+
+function darkMode(){
+    let target = document.getElementById("text1");
+    let elt = document.getElementsByTagName('body').item(0);
+    elt.style.backgroundColor = 'black';
+    target.style.color= "white";
+    target.style.fontFamily = "Times New Roman, serif"
+}
+
+function changeThemeMode(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
 }
